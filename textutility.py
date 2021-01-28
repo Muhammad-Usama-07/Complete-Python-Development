3217619d8f3f637037a3fc6486336b766faa6a1e
Muhammad-Usama-07 <51862131+Muhammad-Usama-07@users.noreply.github.com>
2021-01-28 09:45:34 +0500

    using return in a function

--- textutility.py
+++ textutility.py
@@ -4,7 +4,7 @@ def remove_punctuation(your_string):
     for i in your_string:
         if i in string.punctuation:
             your_string = your_string.replace(i,'')
-    print(your_string)
+    return your_string
 def remove_multiple_spaces(your_string):
     your_string = re.sub(' \s+', ' ', your_string)
     return  your_string
