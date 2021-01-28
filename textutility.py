f08d87513c590d877af65edeb4c8b49f88c7c657
Muhammad-Usama-07 <51862131+Muhammad-Usama-07@users.noreply.github.com>
2021-01-28 09:45:08 +0500

    remove multiple spaces function
    succcessfully made (remove multiple spaces in a string function) in another module) and calling in that function in another file

--- textutility.py
+++ textutility.py
@@ -6,4 +6,5 @@ def remove_punctuation(your_string):
             your_string = your_string.replace(i,'')
     print(your_string)
 def remove_multiple_spaces(your_string):
-    pass
+    your_string = re.sub(' \s+', ' ', your_string)
+    return  your_string
