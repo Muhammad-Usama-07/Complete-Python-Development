13933f71f7adf6bc2317210412c1a2c481d2da5e
Muhammad-Usama-07 <51862131+Muhammad-Usama-07@users.noreply.github.com>
2021-01-28 09:39:31 +0500

    creating def remove_multiple_spaces() function in another module

--- textutility.py
+++ textutility.py
@@ -4,3 +4,5 @@ def remove_punctuation(your_string):
         if i in string.punctuation:
             your_string = your_string.replace(i,'')
     print(your_string)
+def remove_multiple_spaces(your_string):
+    pass
