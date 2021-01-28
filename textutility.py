67286031ec0c85da14ed86f28526ecfc7cb1328c
Muhammad-Usama-07 <51862131+Muhammad-Usama-07@users.noreply.github.com>
2021-01-28 09:29:19 +0500

    making remoive punctuation function in a module

--- textutility.py
+++ textutility.py
@@ -1,3 +1,6 @@
 import string
-def remove_punctuation():
-    print(string.punctuation)
+def remove_punctuation(your_string):
+    for i in your_string:
+        if i in string.punctuation:
+            your_string = your_string.replace(i,'')
+    print(your_string)
